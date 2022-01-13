3cdd8d0a158e703d99ad1c2d8d0d41b6b290c15c
Rus <de.mr44@gmail.com>
2022-01-13 22:46:45 +0500

    Initial Commit

---
+++ Patterns-Decorator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Patterns-Decorator
+//
+//  Created by Ruslan on 13.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
