a2d0475b6c5ba77e2c83122ca107bff349d680b1
Rus <de.mr44@gmail.com>
2022-01-13 23:45:53 +0500

    Added GolfProtocol

---
+++ Patterns-Decorator/GolfProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  GolfProtocol.swift
+//  Patterns-Decorator
+//
+//  Created by Ruslan on 13.01.2022.
+//
+
+import Foundation
+
+protocol GolfProtocol {
+    func getTitle() -> String
+    func getPrice() -> Double
+}
