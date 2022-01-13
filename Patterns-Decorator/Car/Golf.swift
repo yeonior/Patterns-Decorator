51131f68436934096f5b8cab21af4d6d2283420b
Rus <de.mr44@gmail.com>
2022-01-13 23:49:43 +0500

    Added Golf class

---
+++ Patterns-Decorator/Car/Golf.swift
@@ -0,0 +1,19 @@
+//
+//  Golf.swift
+//  Patterns-Decorator
+//
+//  Created by Ruslan on 13.01.2022.
+//
+
+import Foundation
+
+class Golf: GolfProtocol {
+    
+    func getTitle() -> String {
+        "Golf 8 Life"
+    }
+    
+    func getPrice() -> Double {
+        20000.0
+    }
+}
