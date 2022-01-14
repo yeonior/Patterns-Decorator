9d508131fc992e1609b30d6f671a5088b3366b36
Rus <de.mr44@gmail.com>
2022-01-14 19:33:31 +0500

    Added StyleGolf class

---
+++ Patterns-Decorator/Decorator/StyleGolf.swift
@@ -0,0 +1,23 @@
+//
+//  StyleGolf.swift
+//  Patterns-Decorator
+//
+//  Created by Ruslan on 14.01.2022.
+//
+
+import Foundation
+
+class StyleGolf: GolfDecorator {
+    
+    required init(decorator: GolfProtocol) {
+        super.init(decorator: decorator)
+    }
+    
+    override func getTitle() -> String {
+        super.getTitle() + "" + "Style"
+    }
+    
+    override func getPrice() -> Double {
+        super.getPrice() + 7000.0
+    }
+}
